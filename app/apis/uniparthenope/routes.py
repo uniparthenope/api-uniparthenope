61ea45d0ef80e3d23aa0f023f47a21b3a76745cf
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-05-22 19:36:58 +0200

    added new apis

--- app/apis/uniparthenope/routes.py
+++ app/apis/uniparthenope/routes.py
@@ -9,9 +9,12 @@ ns = api.namespace('UniparthenopeApp', description='UniParthenope App operations
 from app.apis.uniparthenope.v1.login_v1 import Login, Logout
 from app.apis.uniparthenope.v1.students_v1 import GetPianoId, GetAverage, GetTotalExams, GetExams, CheckExam, CheckAppello, CheckPrenotazione, getReservations, ExamsToFreq, getProfessors
 from app.apis.uniparthenope.v1.professor_v1 import getCourses
+from app.apis.uniparthenope.v1.general_v1 import CurrentAA, RecentAD, InfoCourse
 
 ns.add_resource(Login, '/v1/login', methods=['GET'])
 ns.add_resource(Logout, '/v1/logout', methods=['GET'])
+
+
 ns.add_resource(GetPianoId, '/v1/students/pianoId/<stuId>', methods=['GET'])
 ns.add_resource(GetAverage, '/v1/students/average/<matId>/<value>', methods=['GET'])
 ns.add_resource(GetTotalExams, '/v1/students/totalExams/<matId>', methods=['GET'])
@@ -25,3 +28,8 @@ ns.add_resource(getProfessors, '/v1/students/getProfessors/<aaId>/<cdsId>', meth
 
 
 ns.add_resource(getCourses, '/v1/professor/getCourses/<aaId>', methods=['GET'])
+
+
+ns.add_resource(CurrentAA, '/v1/general/current_aa/<cdsId>', methods=['GET'])
+ns.add_resource(RecentAD, '/v1/general/recentAD/<adId>', methods=['GET'])
+ns.add_resource(InfoCourse, '/v1/general/infoCourse/<adLogId>', methods=['GET'])
---
+++ app/apis/uniparthenope/v1/general_v1.py
@@ -0,0 +1,179 @@
+from app.apis.uniparthenope.v1.login_v1 import token_required
+from flask import g
+from app import api
+from flask_restplus import Resource
+import requests
+from datetime import datetime, timedelta
+
+url = "https://uniparthenope.esse3.cineca.it/e3rest/api/"
+ns = api.namespace('uniparthenope')
+
+
+def extractData(data):
+    data_split = data.split()[0]
+    export_data = datetime.strptime(data_split, '%d/%m/%Y')
+
+    return export_data
+
+
+# ------------- ANNO ACCADEMICO -------------
+
+
+parser = api.parser()
+parser.add_argument('cdsId', type=str, required=True, help='User cdsId')
+
+
+class CurrentAA(Resource):
+    @ns.doc(security='Basic Auth')
+    @token_required
+    def get(self, cdsId):
+        """Get current AA"""
+
+        headers = {
+            'Content-Type': "application/json",
+            "Authorization": "Basic " + g.token
+        }
+
+        try:
+            response = requests.request("GET", url + "calesa-service-v1/sessioni?cdsId=" + cdsId, headers=headers)
+            _response = response.json()
+            print(response)
+
+            date = datetime.today()
+            curr_day = datetime(date.year, date.month, date.day)
+
+            max_year = 0
+            for i in range(0, len(_response)):
+                if _response[i]['aaSesId'] > max_year:
+                    max_year = _response[i]['aaSesId']
+
+            for i in range(0, len(_response)):
+                if _response[i]['aaSesId'] == max_year:
+                    startDate = extractData(_response[i]['dataInizio'])
+                    endDate = extractData(_response[i]['dataFine'])
+
+                    if (curr_day >= startDate and curr_day <= endDate):
+                        print("Inizio: " + str(startDate))
+                        print("Fine: " + str(endDate))
+                        print("Oggi: " + str(curr_day))
+
+                        curr_sem = _response[i]['des']
+
+                        academic_year = str(_response[i]['aaSesId']) + " - " + str(_response[i]['aaSesId'] + 1)
+
+                        if curr_sem == "Sessione Estiva":
+                            return {
+                                'curr_sem': _response[i]['des'],
+                                'semestre': "Secondo Semestre",
+                                'aa_accad': academic_year
+                            }, 200
+                        else:
+                            return {
+                                'curr_sem': _response[i]['des'],
+                                'semestre': "Primo Semestre",
+                                'aa_accad': academic_year
+                            }, 200
+
+        except requests.exceptions.HTTPError as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.ConnectionError as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.Timeout as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.RequestException as e:
+            return {'errMsg': e}, 500
+        except:
+            return {'errMsg': 'generic error'}, 500
+
+
+# ------------- ANNO ACCADEMICO -------------
+
+
+parser = api.parser()
+parser.add_argument('adId', type=str, required=True, help='User adId')
+
+
+class RecentAD(Resource):
+    @ns.doc(security='Basic Auth')
+    @token_required
+    def get(self, adId):
+        """Get recent AD"""
+
+        headers = {
+            'Content-Type': "application/json",
+            "Authorization": "Basic " + g.token
+        }
+        try:
+            response = requests.request("GET", url + "logistica-service-v1/logistica?adId=" + adId, headers=headers)
+            _response = response.json()
+
+            max_year = 0
+            if response.status_code == 200:
+                for i in range(0, len(_response)):
+                    if _response[i]['chiaveADFisica']['aaOffId'] > max_year:
+                        max_year = _response[i]['chiaveADFisica']['aaOffId']
+
+                for i in range(0, len(_response)):
+                    if _response[i]['chiaveADFisica']['aaOffId'] == max_year:
+                        return {'adLogId': _response[i]['chiavePartizione']['adLogId'],
+                                        'inizio': _response[i]['dataInizio'].split()[0],
+                                        'fine': _response[i]['dataFine'].split()[0],
+                                        'ultMod': _response[i]['dataModLog'].split()[0]
+                                        },200
+            else:
+                return {'stsErr': "N"}, 500
+
+        except requests.exceptions.HTTPError as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.ConnectionError as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.Timeout as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.RequestException as e:
+            return {'errMsg': e}, 500
+        except:
+            return {'errMsg': 'generic error'}, 500
+
+
+# ------------- ANNO ACCADEMICO -------------
+
+
+parser = api.parser()
+parser.add_argument('adLogId', type=str, required=True, help='Exam adLogId')
+
+
+class InfoCourse(Resource):
+    @ns.doc(security='Basic Auth')
+    @token_required
+    def get(self, adLogId):
+        """Get info course"""
+
+        headers = {
+            'Content-Type': "application/json",
+            "Authorization": "Basic " + g.token
+        }
+
+        try:
+            response = requests.request("GET", url + "logistica-service-v1/logistica/" + adLogId + "/adLogConSyllabus", headers=headers)
+            _response = response.json()
+
+            if response.status_code == 200:
+                return {'contenuti': _response[0]['SyllabusAD'][0]['contenuti'],
+                            'metodi': _response[0]['SyllabusAD'][0]['metodiDidattici'],
+                            'verifica': _response[0]['SyllabusAD'][0]['modalitaVerificaApprendimento'],
+                            'obiettivi': _response[0]['SyllabusAD'][0]['obiettiviFormativi'],
+                            'prerequisiti': _response[0]['SyllabusAD'][0]['prerequisiti'],
+                            'testi': _response[0]['SyllabusAD'][0]['testiRiferimento'],
+                            'altro': _response[0]['SyllabusAD'][0]['altreInfo']
+                },200
+
+        except requests.exceptions.HTTPError as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.ConnectionError as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.Timeout as e:
+            return {'errMsg': e}, 500
+        except requests.exceptions.RequestException as e:
+            return {'errMsg': e}, 500
+        except:
+            return {'errMsg': 'generic error'}, 500
