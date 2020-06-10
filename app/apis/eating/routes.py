d62455c198b5879e535f207639a0143cf1cb94e5
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-06-10 19:05:08 +0200

    API restaurants

--- app/apis/eating/routes.py
+++ app/apis/eating/routes.py
@@ -4,8 +4,10 @@ version = "1.0"
 ver = "v1"
 url = "https://uniparthenope.esse3.cineca.it/e3rest/api/"
 
-ns = api.namespace('eating', description='Eating App operations')
+ns = api.namespace('Eating', description='Eating App operations')
 
-from app.apis.eating.v1.eating_v1 import newUser
+from app.apis.eating.v1.eating_v1 import newUser, getUsers, getToday
 
 ns.add_resource(newUser, '/v1/newUser', methods=['POST'])
+ns.add_resource(getUsers, '/v1/getUsers', methods=['GET'])
+ns.add_resource(getToday, '/v1/getToday', methods=['GET'])
--- app/apis/eating/v1/eating_v1.py
+++ app/apis/eating/v1/eating_v1.py
@@ -1,12 +1,13 @@
 import base64
+from datetime import datetime
 
 from app.apis.uniparthenope.v1.login_v1 import token_required_general
 from flask import g, request
 from app import api
 from flask_restplus import Resource, fields
-import requests
 
 from app.models import User
+from app.apis.eating.models import UserFood, Food
 
 url = "https://uniparthenope.esse3.cineca.it/e3rest/api/"
 ns = api.namespace('uniparthenope')
@@ -18,7 +19,8 @@ user = ns.model("user credentials",
                     "user": fields.String(description="username", required=True),
                     "pass": fields.String(description="password", required=True)
                 }
-)
+                )
+
 
 class newUser(Resource):
     @ns.doc(security='Basic Auth')
@@ -40,9 +42,64 @@ class newUser(Resource):
                     for x in user.roles:
                         if x.role == 'admin':
                             print("admin")
+
                 else:
                     return {'errMsg': 'No admin'}, 403
             else:
                 return {'errMsg': 'Missing username/password'}, 502
+        else:
+            return {'errMsg': 'Wrong username/pass'}, g.status
+
+
+# ------------- GET USERS NAME BAR -------------
+
+class getUsers(Resource):
+    @ns.doc(security='Basic Auth')
+    @token_required_general
+    def get(self):
+        if g.status == 200:
+            array = []
+
+            users = UserFood.query.all()
+            for f in users:
+                array.append(f.nome_bar)
+            return array, 200
+        else:
+            return {'errMsg': 'Wrong username/pass'}, g.status
+
+
+# ------------- GET TODAY MENU -------------
+
+class getToday(Resource):
+    @ns.doc(security='Basic Auth')
+    @token_required_general
+    def get(self):
+
+        if g.status == 200:
+            array = []
+            today = datetime.today()
+
+            foods = Food.query.all()
+
+            for f in foods:
+                if (f.data.year == today.year \
+                    and f.data.month == today.month \
+                    and f.data.day == today.day) \
+                        or f.sempre_attivo:
+                    if f.image is None:
+                        image = ""
+                    else:
+                        image = (f.image).decode('ascii')
+
+                    menu = ({'nome': f.nome,
+                             'descrizione': f.descrizione,
+                             'prezzo': f.prezzo,
+                             'tipologia': f.tipologia,
+                             'sempre_attivo': f.sempre_attivo,
+                             'nome_bar': f.user_username,
+                             'image': image})
+                    array.append(menu)
+
+            return array, 200
         else:
             return {'errMsg': 'Wrong username/pass'}, g.status
