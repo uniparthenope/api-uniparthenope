894e5065812e7e7e27ae89b2cc98524a93ebac44
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-09-01 16:33:57 +0200

    database access

--- app/apis/access/models.py
+++ app/apis/access/models.py
@@ -5,12 +5,6 @@ class UserAccess(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     classroom = db.Column(db.String(16))
-    grpId = db.Column(db.Integer)
-    persId = db.Column(db.Integer)
-    stuId = db.Column(db.Integer)
-    matId = db.Column(db.Integer)
-    matricola = db.Column(db.String(16))
-    cdsId = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User {}>'.format(self.username) + '<classroom {}>'.format(self.classroom)
