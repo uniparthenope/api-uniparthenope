472354469aef0ce4bd77cadaf54b2f48d6866970
cirodevita <ciro.devita001@gmail.com>
2020-09-11 00:26:00 +0200

    covidStatementMessage url

--- app/apis/access/routes.py
+++ app/apis/access/routes.py
@@ -10,4 +10,4 @@ from app.apis.access.v1.access_v1 import Access, getCompleteCSV, CovidAlert, Cer
 ns.add_resource(Access, '/v1/classroom', methods=['GET', 'POST'])
 ns.add_resource(Certification, '/v1/covidStatement', methods=['GET', 'POST'])
 ns.add_resource(getCompleteCSV, '/v1/getCSV', methods=['GET'])
-ns.add_resource(CovidAlert, '/v1/covidAlert', methods=['GET'])
+ns.add_resource(CovidAlert, '/v1/covidStatementMessage', methods=['GET'])
