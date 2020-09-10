abed1bc3b98dbc51f8e1a415db2c44d37cab0a43
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-09-11 00:21:21 +0200

    autocertification API

--- app/apis/access/routes.py
+++ app/apis/access/routes.py
@@ -8,6 +8,6 @@ ns = api.namespace('Access', description='UniParthenope access operations')
 from app.apis.access.v1.access_v1 import Access, getCompleteCSV, CovidAlert, Certification
 
 ns.add_resource(Access, '/v1/classroom', methods=['GET', 'POST'])
-ns.add_resource(Certification, '/v1/certification', methods=['GET', 'POST'])
+ns.add_resource(Certification, '/v1/covidStatement', methods=['GET', 'POST'])
 ns.add_resource(getCompleteCSV, '/v1/getCSV', methods=['GET'])
 ns.add_resource(CovidAlert, '/v1/covidAlert', methods=['GET'])
