6a08c8e4d79a1ccb3ac157c40dc7230383112f40
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-09-10 17:27:31 +0200

    new database ristoranti

--- app/apis/eating/models.py
+++ app/apis/eating/models.py
@@ -2,15 +2,32 @@ from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+class Ristorante(db.Model):
+    __bind_key__ = 'eating'
+    __tablename__ = 'ristoranteFood'
+    id = db.Column(db.Integer, primary_key=True)
+    nome_bar = db.Column(db.String(64), index=True, unique=True)
+    telefono = db.Column(db.String(64), unique=True)
+    p_iva = db.Column(db.String(64), unique=True)
+    luogo = db.Column(db.String(64))
+    email = db.Column(db.String(64))
+    image = db.Column(db.BLOB)
+    bar = db.Column(db.String(120), index=True, unique=True)
+    users = db.relationship('UserFood', backref='ristoranteFood')
+
 class UserFood(db.Model):
     __bind_key__ = 'eating'
     __tablename__ = 'userFood'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    image = db.Column(db.BLOB)
+    grpDes = db.Column(db.String(16))
+    grpId = db.Column(db.Integer)
     email = db.Column(db.String(120), index=True, unique=True)
-    nome_bar = db.Column(db.String(120), index=True, unique=True)
+    user = db.Column(db.String(120), index=True, unique=True)
     foods = db.relationship('Food', backref='userFood')
+    bar = db.Column(db.String(120), db.ForeignKey('ristoranteFood.bar'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username) + '<foods {}>'.format(self.foods)
@@ -33,7 +50,7 @@ class Food(db.Model):
     prezzo = db.Column(db.Integer)
     sempre_attivo = db.Column(db.Boolean)
     data = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_username = db.Column(db.String(120), db.ForeignKey('userFood.nome_bar'))
+    user_username = db.Column(db.String(120), db.ForeignKey('userFood.user'))
 
     def __repr__(self):
         return '<Nome {}>'.format(self.nome)
