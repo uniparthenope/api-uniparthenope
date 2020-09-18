95d8ff5eec43bd3dd53b8f9070fbe0f886ea02d6
ciro97 <cirogiuseppe.devita001@studenti.uniparthenope.it>
2020-09-18 19:31:17 +0200

    ga database

--- app/apis/ga_uniparthenope/models.py
+++ app/apis/ga_uniparthenope/models.py
@@ -2,13 +2,15 @@ from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class Building(db.Model):
+
+class Reservations(db.Model):
     __bind_key__ = 'ga'
-    id_corso = db.Column(db.Integer, primary_key=True)
-    struttura_des = db.Column(db.String(120))
-    struttura_id = db.Column(db.String(10))
-    struttura_ga_id = db.Column(db.Integer)
-    corso_ga_id = db.Column(db.String(10))
+    __tablename__ = 'reservations'
+    id = db.Column(db.Integer, primary_key=True)
+    id_corso = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(120), nullable=False)
+    time = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
+    id_lezione =  db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<Id Corso {}>'.format(self.id_corso)
+        return '<Id Lezione{}>'.format(self.id_lezione) + '<Id Corso{}>'.format(self.id_corso) + '<username{}>'.format(self.username)
