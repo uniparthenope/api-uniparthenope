2788a830d562f88c41c2f005b183dd97a00c80a2
cirodevita <ciro.devita001@gmail.com>
2020-09-29 12:27:03 +0200

    new badge API (v2)

--- app/apis/badges/routes.py
+++ app/apis/badges/routes.py
@@ -6,7 +6,9 @@ ver = "v1"
 ns = api.namespace('Badges', description='UniParthenope App operations')
 
 from app.apis.badges.v1.badges_v1 import QrCode, QrCodeCheck, QrCodeStatus
+from app.apis.badges.v2.badges_v2 import QrCodeCheck_v2
 
 ns.add_resource(QrCode, '/v1/generateQrCode', methods=['GET'])
 ns.add_resource(QrCodeCheck, '/v1/checkQrCode', methods=['POST'])
+ns.add_resource(QrCodeCheck_v2, '/v2/checkQrCode', methods=['POST'])
 ns.add_resource(QrCodeStatus, '/v1/QrCodeStatus/<int:interval>/<tabletId>', methods=['GET'])
