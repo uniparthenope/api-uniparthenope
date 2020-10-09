ad29feadc57472e6f6cc6b8f66796758d7d13361
cirodevita <39745066+cirodevita@users.noreply.github.com>
2020-10-09 12:06:06 +0200

    Delete demo.py
    test

--- app/apis/uniparthenope/demo.py
+++
@@ -1,7 +0,0 @@
-users_demo = {
-    'c3R1ZGVudC5kZW1vOlBhcnRoZW5vcGUyMDIwIURlbW9JT1M=': 'Z2VubmFyby5tZWxsb25lOnNob3R0eTkw', #studente fittizio
-    'Z2VubmFyby5tZWxsb25lOlBhcnRoZW5vcGUyMDIwIURlbW9JT1M=': 'Z2VubmFyby5tZWxsb25lOnNob3R0eTkw', #studente fittizio
-    'cHJvZi5kZW1vOlBhcnRoZW5vcGUyMDIwIURlbW9JT1M=': 'cmFmZmFlbGUubW9udGVsbGE6U2FyaW1hNDRpdiQk', #prof fittizio
-    'cmFmZmFlbGUubW9udGVsbGE6UGFydGhlbm9wZTIwMjAhRGVtb0lPUw==': 'cmFmZmFlbGUubW9udGVsbGE6U2FyaW1hNDRpdiQk' #prof fittizio
-    #ristoratore fittizio
-}
