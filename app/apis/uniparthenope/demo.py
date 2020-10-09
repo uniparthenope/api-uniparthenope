9e272c4be7e9ee4766516252c4c0ef09c95e96c1
cirodevita <ciro.devita001@gmail.com>
2020-10-09 12:06:34 +0200

    test

--- app/apis/uniparthenope/demo.py
+++ app/apis/uniparthenope/demo.py
@@ -3,5 +3,6 @@ users_demo = {
     'Z2VubmFyby5tZWxsb25lOlBhcnRoZW5vcGUyMDIwIURlbW9JT1M=': 'Z2VubmFyby5tZWxsb25lOnNob3R0eTkw', #studente fittizio
     'cHJvZi5kZW1vOlBhcnRoZW5vcGUyMDIwIURlbW9JT1M=': 'cmFmZmFlbGUubW9udGVsbGE6U2FyaW1hNDRpdiQk', #prof fittizio
     'cmFmZmFlbGUubW9udGVsbGE6UGFydGhlbm9wZTIwMjAhRGVtb0lPUw==': 'cmFmZmFlbGUubW9udGVsbGE6U2FyaW1hNDRpdiQk' #prof fittizio
+    
     #ristoratore fittizio
 }
