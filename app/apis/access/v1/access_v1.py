bd61f36a91158677604ff9c4343336c5ab51ce23
cirodevita <Anomalo97>
2020-10-21 21:37:39 +0200

    bug fixed

--- app/apis/access/v1/access_v1.py
+++ app/apis/access/v1/access_v1.py
@@ -16,7 +16,6 @@ from app.models import User, Role
 from app.apis.access.models import UserAccess
 from app.log.log import time_log
 
-
 url = "https://uniparthenope.esse3.cineca.it/e3rest/api/"
 ns = api.namespace('uniparthenope')
 
@@ -39,83 +38,80 @@ class Access(Resource):
 
         if g.status == 200:
             try:
-                base64_bytes = g.token.encode('utf-8')
-                message_bytes = base64.b64decode(base64_bytes)
-                token_string = message_bytes.decode('utf-8')
-                userId = token_string.split(':')[0]
-
                 r = g.response
 
-                if 'accessType' in content:
-                    print(content['accessType'])
-                    if content['accessType'] == 'presence' or content['accessType'] == 'distance' or content[
-                        'accessType'] == 'undefined':
-                        user = UserAccess.query.filter_by(username=userId).first()
+                userId = r['user']['userId']
 
-                        if r['user']['grpId'] == 6:
-                            if r['user']['userId'] != userId:
-                                user = UserAccess.query.filter_by(username=r['user']['userId']).first()
+                if r['user']['grpId'] == 7 or r['user']['grpId'] == 99 or r['user']['grpId'] == 97 or r['user']['grpId'] == 98:
+                    return {'errMsg': 'Operazione non consentita!'}, 403
+                else:
+                    if 'accessType' in content:
+                        if content['accessType'] == 'presence' or content['accessType'] == 'distance' or content[
+                            'accessType'] == 'undefined':
+                            user = UserAccess.query.filter_by(username=userId).first()
 
-                        if user is None:
-                            try:
-                                if content['accessType'] == "presence" and (user.autocertification == False or user.autocertification is None):
-                                    return {'errMsg': 'Permesso negato. Autocertificazione mancante!'}, 500
+                            if user is None:
+                                try:
+                                    if content['accessType'] == "presence" and (
+                                            user.autocertification == False or user.autocertification is None):
+                                        return {'errMsg': 'Permesso negato. Autocertificazione mancante!'}, 500
 
-                                else:
-                                    if r['user']['grpId'] == 6:
-                                        u = UserAccess(username=r['user']['userId'], classroom=content['accessType'],
-                                                   grpId=r['user']['grpId'], persId=r['user']['persId'],
-                                                   stuId=r['user']['trattiCarriera'][0]['stuId'],
-                                                   matId=r['user']['trattiCarriera'][0]['matId'],
-                                                   matricola=r['user']['trattiCarriera'][0]['matricola'],
-                                                   cdsId=r['user']['trattiCarriera'][0]['cdsId'])
-                                    elif r['user']['grpId'] == 7:
-                                        u = UserAccess(username=userId, classroom=content['accessType'],
-                                                   grpId=r['user']['grpId'], persId=r['user']['docenteId'], stuId=None,
-                                                   matId=None, matricola="", cdsId=None)
                                     else:
-                                        u = UserAccess(username=userId, classroom=content['accessType'],
-                                                   grpId=None, persId=None,
-                                                   stuId=None, matId=None, matricola="", cdsId=None)
-                                    db.session.add(u)
-                                    db.session.commit()
+                                        if r['user']['grpId'] == 6:
+                                            u = UserAccess(username=userId,
+                                                           classroom=content['accessType'],
+                                                           grpId=r['user']['grpId'], persId=r['user']['persId'],
+                                                           stuId=r['user']['trattiCarriera'][0]['stuId'],
+                                                           matId=r['user']['trattiCarriera'][0]['matId'],
+                                                           matricola=r['user']['trattiCarriera'][0]['matricola'],
+                                                           cdsId=r['user']['trattiCarriera'][0]['cdsId'])
+                                        else:
+                                            u = UserAccess(username=userId, classroom=content['accessType'],
+                                                           grpId=None, persId=None,
+                                                           stuId=None, matId=None, matricola="", cdsId=None)
+                                        db.session.add(u)
+                                        db.session.commit()
 
-                                    return {'message': 'Classroom modified'}, 200
+                                        return {'message': 'Classroom modified'}, 200
 
-                            except AttributeError:
-                                return {
-                                           'errMsgTitle': 'Attenzione',
-                                           'errMsg': 'Non è possibile scegliere la modalità "in presenza" senza aver accettato l\'autocertificazione obbligatoria!'
-                                       }, 500
+                                except AttributeError:
+                                    db.session.rollback()
+                                    return {
+                                               'errMsgTitle': 'Attenzione',
+                                               'errMsg': 'Non è possibile scegliere la modalità "in presenza" senza aver accettato l\'autocertificazione obbligatoria!'
+                                           }, 500
 
-                            except:
-                                print("Unexpected error:")
-                                print("Title: " + sys.exc_info()[0].__name__)
-                                print("Description: " + traceback.format_exc())
-                                return {
-                                           'errMsgTitle': sys.exc_info()[0].__name__,
-                                           'errMsg': traceback.format_exc()
-                                       }, 500
+                                except:
+                                    db.session.rollback()
+                                    print("Unexpected error:")
+                                    print("Title: " + sys.exc_info()[0].__name__)
+                                    print("Description: " + traceback.format_exc())
+                                    return {
+                                               'errMsgTitle': sys.exc_info()[0].__name__,
+                                               'errMsg': traceback.format_exc()
+                                           }, 500
+                            else:
+                                try:
+                                    if content['accessType'] == "presence" and (
+                                            user.autocertification == False or user.autocertification is None):
+                                        return {'errMsg': 'Permesso negato. Autocertificazione mancante!'}, 500
+                                    else:
+                                        user.classroom = content['accessType']
+                                        db.session.commit()
+                                        return {'message': 'Classroom modified'}, 200
+                                except:
+                                    db.session.rollback()
+                                    print("Unexpected error:")
+                                    print("Title: " + sys.exc_info()[0].__name__)
+                                    print("Description: " + traceback.format_exc())
+                                    return {
+                                               'errMsgTitle': sys.exc_info()[0].__name__,
+                                               'errMsg': traceback.format_exc()
+                                           }, 500
                         else:
-                            try:
-                                if content['accessType'] == "presence" and (user.autocertification == False or user.autocertification is None):
-                                    return {'errMsg': 'Permesso negato. Autocertificazione mancante!'}, 500
-                                else:
-                                    user.classroom = content['accessType']
-                                    db.session.commit()
-                                    return {'message': 'Classroom modified'}, 200
-                            except:
-                                print("Unexpected error:")
-                                print("Title: " + sys.exc_info()[0].__name__)
-                                print("Description: " + traceback.format_exc())
-                                return {
-                                           'errMsgTitle': sys.exc_info()[0].__name__,
-                                           'errMsg': traceback.format_exc()
-                                       }, 500
+                            return {'errMsg': 'Wrong body!'}, 500
                     else:
                         return {'errMsg': 'Wrong body!'}, 500
-                else:
-                    return {'errMsg': 'Wrong body!'}, 500
             except:
                 print("Unexpected error:")
                 print("Title: " + sys.exc_info()[0].__name__)
@@ -127,21 +123,17 @@ class Access(Resource):
         else:
             return {'errMsg': 'Wrong username/pass'}, g.status
 
-
     @time_log(title="ACCESS_V1", filename="access_v1.log", funcName="Access GET")
     @ns.doc(security='Basic Auth')
     @token_required_general
     def get(self):
         """Get classroom"""
 
-        if g.status == 200 or g.status == 202:
+        if g.status == 200:
             try:
-                base64_bytes = g.token.encode('utf-8')
-                message_bytes = base64.b64decode(base64_bytes)
-                token_string = message_bytes.decode('utf-8')
-                userId = token_string.split(':')[0]
-
                 r = g.response
+
+                userId = r['user']['userId']
 
                 user = UserAccess.query.filter_by(username=userId).first()
 
@@ -196,29 +188,22 @@ class Certification(Resource):
 
         content = request.json
 
-        if g.status == 200 or g.status == 202:
+        if g.status == 200:
             try:
-                base64_bytes = g.token.encode('utf-8')
-                message_bytes = base64.b64decode(base64_bytes)
-                token_string = message_bytes.decode('utf-8')
-                userId = token_string.split(':')[0]
-
                 r = g.response
 
+                userId = r['user']['userId']
+
                 if 'covidStatement' in content:
-                    print(content['covidStatement'])
                     if content['covidStatement'] == True or content['covidStatement'] == False:
                         user = UserAccess.query.filter_by(username=userId).first()
-
-                        if r['user']['grpId'] == 6:
-                            if r['user']['userId'] != userId:
-                                user = UserAccess.query.filter_by(username=r['user']['userId']).first()
 
                         if user is None:
                             try:
                                 if r['user']['grpId'] == 6:
                                     u = UserAccess(username=r['user']['userId'], classroom="undefined",
-                                                   autocertification=content['covidStatement'], grpId=r['user']['grpId'],
+                                                   autocertification=content['covidStatement'],
+                                                   grpId=r['user']['grpId'],
                                                    persId=r['user']['persId'],
                                                    stuId=r['user']['trattiCarriera'][0]['stuId'],
                                                    matId=r['user']['trattiCarriera'][0]['matId'],
@@ -231,7 +216,7 @@ class Certification(Resource):
                                                    matId=None, matricola="", cdsId=None)
                                 else:
                                     u = UserAccess(username=userId, classroom="undefined",
-                                                   autocertification=content['covidStatement'], grpId=None, persId=None,
+                                                   autocertification=content['covidStatement'], grpId=r['user']['grpId'], persId=None,
                                                    stuId=None, matId=None, matricola="", cdsId=None)
                                 db.session.add(u)
                                 db.session.commit()
@@ -248,9 +233,8 @@ class Certification(Resource):
                                        }, 500
                         else:
                             try:
-                                if r['user']['grpId'] != 7 or r['user']['grpId'] != 99 or r['user']['grpId'] != 97:
-                                    if user.classroom == "presence" and content['covidStatement'] == False:
-                                        return {'errMsg': 'Operazione non consetita. Si è scelto di seguire in presenza!'}, 500
+                                if user.classroom == "presence" and content['covidStatement'] == False:
+                                    return {'errMsg': 'Operazione non consetita. Si è scelto di seguire in presenza!'}, 500
                                 else:
                                     user.autocertification = content['covidStatement']
                                     db.session.commit()
@@ -283,14 +267,11 @@ class Certification(Resource):
     def get(self):
         """Get covidStatement"""
 
-        if g.status == 200 or g.status == 202:
+        if g.status == 200:
             try:
-                base64_bytes = g.token.encode('utf-8')
-                message_bytes = base64.b64decode(base64_bytes)
-                token_string = message_bytes.decode('utf-8')
-                userId = token_string.split(':')[0]
-
                 r = g.response
+
+                userId = r['user']['userId']
 
                 user = UserAccess.query.filter_by(username=userId).first()
 
@@ -355,8 +336,8 @@ class getCompleteCSV(Resource):
                         writer = csv.writer(data)
 
                         writer.writerow((
-                                        "username", "grpId", "persId/docenteId", "stuId", "matId", "cdsId", "matricola",
-                                        "scelta", "dichiarazione"))
+                            "username", "grpId", "persId/docenteId", "stuId", "matId", "cdsId", "matricola",
+                            "scelta", "dichiarazione"))
                         yield data.getvalue()
                         data.seek(0)
                         data.truncate(0)
@@ -397,10 +378,10 @@ class CovidAlert(Resource):
 
         return {
                    "title": "DICHIARAZIONE PER IL CONTENIMENTO DEL COVID-19",
-                   "body": '<div>In relazione a quanto previsto dalle Linee guida per il contenimento della diffusione del covid-19 in materia di accesso alle sedi universitarie, confermo</div>'+
-                            '<ul>' +
-                                '<li>di non essere affetto da COVID-19 o di non essere stato sottoposto a periodo di quarantena obbligatoria di almeno 14 giorni;</li>' +
-                                '<li>di non accusare sintomi riconducibili al COVID-19 quali, a titolo esemplificativo, temperatura corporea superiore a 37,5&deg;C, tosse, raffreddore e di non aver avuto contatti con persona affetta da COVID-19 negli ultimi 14 giorni;</li>' +
-                                '<li>l&#39;impegno a rinunciare all&rsquo;accesso alle sedi dell&rsquo;Universit&agrave; degli Studi Parthenope e ad informare l&#39;Autorit&agrave; sanitaria competente nell&#39;ipotesi in cui qualsiasi dei predetti sintomi dovesse emergere prima, durante o dopo la frequentazione delle strutture di questo Ateneo;</li>' +
-                            '</ul>'
+                   "body": '<div>In relazione a quanto previsto dalle Linee guida per il contenimento della diffusione del covid-19 in materia di accesso alle sedi universitarie, confermo</div>' +
+                           '<ul>' +
+                           '<li>di non essere affetto da COVID-19 o di non essere stato sottoposto a periodo di quarantena obbligatoria di almeno 14 giorni;</li>' +
+                           '<li>di non accusare sintomi riconducibili al COVID-19 quali, a titolo esemplificativo, temperatura corporea superiore a 37,5&deg;C, tosse, raffreddore e di non aver avuto contatti con persona affetta da COVID-19 negli ultimi 14 giorni;</li>' +
+                           '<li>l&#39;impegno a rinunciare all&rsquo;accesso alle sedi dell&rsquo;Universit&agrave; degli Studi Parthenope e ad informare l&#39;Autorit&agrave; sanitaria competente nell&#39;ipotesi in cui qualsiasi dei predetti sintomi dovesse emergere prima, durante o dopo la frequentazione delle strutture di questo Ateneo;</li>' +
+                           '</ul>'
                }, 200
