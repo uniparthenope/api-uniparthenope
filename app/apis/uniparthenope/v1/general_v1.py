7a62f21c05c1873835979df30d21295bf3f33d24
g.mellone <mellone.gennaro@hotmail.com>
2021-02-11 14:54:36 +0100

    new privacy API

--- app/apis/uniparthenope/v1/general_v1.py
+++ app/apis/uniparthenope/v1/general_v1.py
@@ -602,7 +602,7 @@ class Privacy(Resource):
         parsed_html = BeautifulSoup(page.content, 'html.parser')
         mydivs = parsed_html.find('div',{"class": "field-item even"})
         mydivs_text = mydivs.getText()
-        mydivs_text = mydivs_text.replace('\n', '<br>')
+        mydivs_text = mydivs_text.replace('\n', '<br/>')
 
         return {'privacy': '<p>' + mydivs_text + '</p>'}, 200
 
