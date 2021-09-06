dcd9f4479cfa2874916f49071107da66a7782ab1
gennaromellone <gennaro.mellone1@studenti.uniparthenope.it>
2021-09-06 20:19:31 +0200

    remove gp

--- app/apis/badges/routes.py
+++ app/apis/badges/routes.py
@@ -7,7 +7,7 @@ ns = api.namespace('Badges', description='UniParthenope App operations')
 
 from app.apis.badges.v1.badges_v1 import QrCode, QrCodeCheck, QrCodeStatus, SyncMachine, ScanHistory
 from app.apis.badges.v2.badges_v2 import QrCode_v2, QrCodeCheck_v2, QrCode_v2_SPID, sendRequestInfo, sendInfo, getContactInfo
-from app.apis.badges.v3.badges_v3 import QrCodeCheck_v3, GreenPassCheck, GreenPassCheckNoScan, ListGreenPass, GreenPassStatus, GreenPassCheckMobile, CheckOperator
+from app.apis.badges.v3.badges_v3 import QrCodeCheck_v3, GreenPassCheck, GreenPassCheckNoScan, ListGreenPass, GreenPassStatus, GreenPassCheckMobile, GreenPassRemove, CheckOperator
 
 ns.add_resource(QrCode, '/v1/generateQrCode', methods=['GET'])
 ns.add_resource(QrCodeCheck, '/v1/checkQrCode', methods=['POST'])
@@ -30,5 +30,7 @@ ns.add_resource(GreenPassCheckMobile, '/v3/checkGreenPassMobile', methods=['POST
 ns.add_resource(GreenPassCheckNoScan, '/v3/checkGreenPassNoScan', methods=['POST'])
 ns.add_resource(ListGreenPass, '/v3/listGreenPass', methods=['GET'])
 ns.add_resource(GreenPassStatus, '/v3/greenPassStatus', methods=['GET'])
+ns.add_resource(GreenPassRemove, '/v3/greenPassRemove', methods=['DELETE'])
+
 
 ns.add_resource(CheckOperator, '/v3/checkOperator', methods=['POST'])
